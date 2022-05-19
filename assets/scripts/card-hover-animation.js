5e4eb5cfff9ad5d6714a40c29db06326ee740469
leticialg <leticialg@ciandt.com>
2022-05-19 09:22:11 -0300

    criação da primeira page do homem aranha 1

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -13,8 +13,8 @@ function cardsHover() {
 
   for (let index = 0; index < cardElements.length; index++) {
     const card = cardElements[index]
-    card.addEventListener('mouseenter', handleMouseEnter)
-    card.addEventListener('mouseleave', handleMouseLeave)
+    card.addEventListener('mouseenter', handleMouseEnter) //quando passa o mouse em cima
+    card.addEventListener('mouseleave', handleMouseLeave) //quando tira o mouse de cima
   }
 }
 
